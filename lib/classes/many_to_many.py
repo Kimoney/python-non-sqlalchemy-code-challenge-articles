b9f08dce7d7ed564ec14aec085b6561b1af85754
John <johntaykimani@gmail.com>
2024-03-11 09:13:16 +0300

    Completed Article & Author

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,22 +1,68 @@
+import ipdb
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if type(title) == str and 5 <= len(title) <= 50:
+            self._title = title
+        else:
+            raise Exception
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if not isinstance(author, Author):
+            raise Exception
+        self._author = author
+
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, magazine):
+        if not isinstance(magazine, Magazine):
+            raise Exception
+        self._magazine = magazine
         
 class Author:
     def __init__(self, name):
         self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) > 0 and not hasattr(self, '_name'):
+            self._name = name
+        else:
+            raise AttributeError("Name must be a string and is immutable")
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
-
+        Article(self, magazine, title)
+        
     def topic_areas(self):
         pass
 
